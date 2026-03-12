6cc436fe5c66c7ba9f5e9eaefa9f599ca3cbfa9c
ilariamotta <ilaria.motta93@gmail.com>
2026-03-12 17:57:42 +0100

    feat: added form

--- index.php
+++ index.php
@@ -20,9 +20,28 @@ $discs = json_decode($json, true);
 </head>
 <body>
 <div class="container mx-auto px-4 py-8">
-    <h1 class="text-center">My Collection</h1>
+    <!-- TITLE -->
+    <h1 class="text-4xl font-bold text-center mb-8">My Collection</h1>
+<!-- FORM -->
+ <form action="server.php" method="POST">
+    <!-- title -->
+    <label>Titolo disco</label>
+<input type="text" name="titolo" class="border border-gray-300 rounded-lg px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-400">
+<!-- artist -->
+<label>Artista</label>
+<input type="text" name="artista" class="border border-gray-300 rounded-lg px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-400">
+<!-- year -->
+<label>Anno</label>
+<input type="number" name="anno" class="border border-gray-300 rounded-lg px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-400">
+<!-- genre -->
+<label>Genere</label>
+<input type="text" name="genere" class="border border-gray-300 rounded-lg px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-400">
+
+
+ </form>
+
     <!-- GRID -->
-    <div class="grid grid-cols-3 gap-6">
+    <div class="grid grid-cols-6 gap-6">
 
         <!-- ALBUM -->
          <?php foreach($discs as $disc){ ?>
